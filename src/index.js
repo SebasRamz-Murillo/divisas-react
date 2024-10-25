2c5c812217bd1804404494fba4383da0687769b3
Sebastian Ramirez <71026252+SebasRamz-Murillo@users.noreply.github.com>
2024-10-24 19:07:48 -0600

    sw

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ root.render(
 );
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('/service-worker.js')
+    navigator.serviceWorker.register('/sw.js')
       .then((registration) => {
         console.log('SW registrado:', registration);
       })
